165e0bd933b480c5a8fa822a6549ec45d86463a7
zlayaa <viktoriazlayaa@gmail.com>
2019-01-17 19:27:06 +0200

    add config

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+set :environment, "production"
+set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
 every 1.minute do
   rake 'send_user_notification'
