8e407ece24f9c29cde99e93b2f739459194b269e
zlayaa <viktoriazlayaa@gmail.com>
2019-01-18 00:53:32 +0200

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 job_type :rake, "cd :path && :environment_variable=:environment :bundle_command rake :task :output"
-set :environment, "development"
+set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
 every 1.minute do
