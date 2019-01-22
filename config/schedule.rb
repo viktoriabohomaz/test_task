fea6f01d00f970ea1214fb8432af97256cc42c05
zlayaa <viktoriazlayaa@gmail.com>
2019-01-22 11:06:25 +0200

    change config/schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
-job_type :rake, "cd :path && :environment_variable=:environment :bundle_command rake :task :output"
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
+env :PATH, ENV['PATH']
 
 every 1.minute do
   rake 'email_tasks:send_user_notification'
